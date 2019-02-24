c9a510e8c71e70e60f788a218742409e22878838
Tristan Peralta <tristanperalta@gmail.com>
2019-02-24 11:59:08 +0800

    Pull data from database

--- datasource.js
+++ datasource.js
@@ -8,24 +8,7 @@ class Task {
   }
 
   async getAllTasks() {
-    return await [
-      { id: 1,
-        note: 'Going to work',
-        createdAt: (new Date(2019, 1, 1)).toISOString()
-      },
-      { id: 2,
-        note: 'Buy food',
-        createdAt: (new Date(2019, 1, 2)).toISOString()
-      },
-      { id: 3,
-        note: 'Cook',
-        createdAt: (new Date(2019, 1, 3)).toISOString()
-      },
-      { id: 4,
-        note: 'Workout',
-        createdAt: (new Date(2019, 2, 1)).toISOString()
-      }
-    ]
+    return this.client.select('*').from('tasks')
   }
 }
 
